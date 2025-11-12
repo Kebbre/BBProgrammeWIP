544f1a54f3b686e99a4fdfb65316c822a7e901c7
Kebbre <a.pescher@bondbryan.co.uk>
2025-11-12 10:46:20 +0000

    01.02.04 adjusted css

--- modules/constants.js
+++ modules/constants.js
@@ -1,6 +1,6 @@
 const TASK_COLOR_MAP = {
-  'Bond Bryan Design': 'rgb(176, 207, 255)',
-  'Bond Bryan review': 'rgb(255, 192, 203)',
+  'Bond Bryan Design': 'rgba(134, 176, 240, 1)',
+  'Bond Bryan review': 'rgba(199, 201, 238, 1)',
   'CDP': 'rgb(196, 235, 214)',
   'Client Comments': 'rgb(255, 223, 186)',
   'Delays': 'rgb(220, 220, 230)',
